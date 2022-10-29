5b2750fb6fd42f52c6e2a84d2e7e638a5f779fab
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-29 23:37:25 +0600

    :hammer: nightmare

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,18 +2,25 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
     menuList: document.querySelector('.mob-menu-nav__list'),
+
+
+    btn: document.querySelector('.btn'),
+    fav: document.querySelector('#favorites-ul'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   refs.menuList.addEventListener('click', removeMenu);
+  refs.btn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    // refs.fav.classList.toggle('is-hidden');
+    // refs.body.classList.toggle('no-scroll');
   }
 
   function removeMenu() {
