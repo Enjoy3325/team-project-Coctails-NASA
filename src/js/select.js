b828cfdfa93e83a11a24d239ab0a835eb0c51e58
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-10-29 15:09:44 +0300

    update request API

--- src/js/select.js
+++ src/js/select.js
@@ -46,8 +46,37 @@ function renderOptions() {
         }</option>`
     )
     .join('');
-  console.log(document.querySelector('#letter'), markup);
+  //   console.log(document.querySelector('#letter'), markup);
   document.querySelector('#letter').insertAdjacentHTML('beforeend', markup);
 }
 
-renderOptions();
+function renderLetterButtons() {
+  let arr = arrLetters;
+  arr.splice(-10, 1, ' ');
+  console.log(arr);
+  const markup = arr
+    .map(
+      letter =>
+        `<button type="button" class="hero__letter" data-letter="${letter}">${
+          !isNaN(letter) ? letter : letter.toUpperCase()
+        }</button>`
+    )
+    .join('');
+  document
+    .querySelector('.hero__letter-box')
+    .insertAdjacentHTML('beforeend', markup);
+}
+console.log(window.innerWidth, window.innerWidth > 479);
+
+function checkWindowWidth() {
+  if (window.innerWidth > 767) {
+    renderLetterButtons();
+  } else {
+    renderOptions();
+  }
+}
+
+checkWindowWidth();
+
+import { requestApi } from './requests-api.js';
+requestApi('a', 'letter').then(data => console.log('data', data));
