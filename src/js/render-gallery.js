dc34f22d41d20823ef1c2186e9652a15fc5c8b94
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-29 09:49:37 +0300

    commit

---
+++ src/js/render-gallery.js
@@ -0,0 +1,7 @@
+const gallery = document.querySelector('.gallery');
+const drinksArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function renderCoctails(cocktailsArr)
+const galleryItemsArr = cocktailsArr.map(() =>
+``
+)
