062092758771a228bfe847f19b902c5c6a6dbd56
Enjoy3325 <enjoy3325@gmail.com>
2022-10-30 21:49:03 +0200

    add render markup dynamic

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
 export const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
+  // openModalBtn: document.querySelector('[data-modal-open]'),
+  openModalBtn: document.querySelectorAll('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
   modal2: document.querySelector('[data-modal-ingridients]'),
@@ -7,39 +8,100 @@ export const refs = {
   body: document.querySelector('body'),
   btnAddFavorie: document.querySelector('.modal-coctails__button'),
   ingridientsItems: document.querySelectorAll('.ingridients__item'),
-  gallery: document.querySelector('.gallery')
+  gallery: document.querySelector('.gallery'),
+  modalBody: document.querySelector('.modal-coctails'),
 };
-<<<<<<< HEAD
-console.log(refs.openModalBtn);
-=======
+// console.log(refs.openModalBtn);
 
-let selectedObj = ''
+let selectedObj = {
+  img: 'https://www.thecocktaildb.com/images/media/drink/rwsyyu1483388181.jpg',
+  instruction:
+    'In a shaker half-filled with ice cubes, combine all of the ingredients. Shake well. Strain into a cocktail glass.',
+  name: 'Almeria',
+};
+// refs.gallery.addEventListener('click', e => {
+//   let selectedName = e.target.dataset.cocktail;
+//   selectedObj = JSON.parse(localStorage.getItem('cocktails')).find(
+//     el => el.name === selectedName
+//   );
 
-refs.gallery.addEventListener('click', e => {
-  let selectedName = e.target.dataset.cocktail
-  selectedObj = JSON.parse(localStorage.getItem('cocktails')).find(el => el.name === selectedName)
-  // console.log(selectedObj);
-})
+// });
 
-// console.log(selectedObj);
+// function onCreateCockteilsMarkup(selectedObj) {
+//   console.log('----> onCreateCockteilsMarkup ', selectedObj.name);
+// }
 
->>>>>>> main
+function onCreateCockteilsMarkup() {
+  return ` <div class="modal-coctails">
+    <button data-modal-close class="modal-coctails__closeModal" type="button">
+      <svg width="18" height="18">
+        <use href="./images/sprite.svg#cross"></use>
+      </svg>
+    </button>
+    <h2 class="modal-coctails__header">${selectedObj.name}</h2>
+
+    <b class="modal-coctails__title">INSTRACTIONS:</b>
+    <p class="modal-coctails__text">${selectedObj.instruction}</p>
+    <div class="modal-coctails__box-image">
+    <img class="modal-coctails__image" src=${selectedObj.img} alt="${selectedObj.name}" />
+    </div>
+    <div class="ingridients">
+      <h3 class="ingridients__subtitle">INGREDIENTS</h3>
+      <p class="ingridients__subtext">Per cocktail</p>
+      <ul class="ingridients__list">
+        <li class="ingridients__item">
+          <span>Ice</span>
+        </li>
+        <li class="ingridients__item">
+          <span>1 ounce gin</span>
+        </li>
+        <li class="ingridients__item">
+          <span>1 ounce Campari</span>
+        </li>
+        <li class="ingridients__item">
+          <span>1 ounce sweet vermouth</span>
+        </li>
+        <li class="ingridients__item">
+          <span>orange peel</span>
+        </li>
+      </ul>
+    </div>
+    <button class="modal-coctails__button" type="button">
+      Add to favorite
+    </button>
+  </div>`;
+}
+
+refs.modalBody.insertAdjacentHTML('beforeend', onCreateCockteilsMarkup());
+console.log(cocktailMarkup);
+
 let flag = true;
 //Слушатели
-refs.openModalBtn.addEventListener('click', toggleModal);
+refs.openModalBtn.addEventListenerAll('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.backdrop.addEventListener('click', onBakdropClick);
 refs.btnAddFavorie.addEventListener('click', onCangeBtn);
 window.addEventListener('keydown', handleCloseModal);
+// refs.openModalBtn.addEventListener('click', toggleModal);
 
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+  refs.body.classList.toggle('no-scroll');
+  refs.modal2.classList.add('is-hidden');
+  onCreateCockteilsMarkup();
+  refs.openModalBtn.forEach(el => {
+    el.addEventListener('click', toggleModal);
+  });
+}
 
 // Открытие 1й модалки с галереи
-function toggleModal(e) {
-  console.log(refs.modal.classList, e);
-  refs.modal.classList.toggle('is-hidden');
-  // refs.body.classList.toggle('no-scroll');
-  // refs.modal2.classList.add('is-hidden');
-}
+// function toggleModal(e) {
+//   console.log(refs.modal.classList, e);
+//   refs.modal.classList.toggle('is-hidden');
+//   // refs.body.classList.toggle('no-scroll');
+//   // refs.modal2.classList.add('is-hidden');
+//   onCreateCockteilsMarkup();
+// }
 //  Закрытие модалки по кнопке
 function onCloseModal() {
   if ('click') {
