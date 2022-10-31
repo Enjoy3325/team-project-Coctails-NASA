1b15f840afe430f26eac2226fd96f27ebde307da
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-31 18:11:55 +0600

    :art: add styles to theme switcher, add favicon

--- src/js/themeChanger.js
+++ src/js/themeChanger.js
@@ -1,4 +1,5 @@
 const slider = document.querySelector('.themeChanger')
+const sliderMob = document.querySelector('.mob-menu-switcher .slider')
 const body = document.querySelector('body')
 
 const themeChanger = () => {
@@ -6,3 +7,4 @@ const themeChanger = () => {
 };
 
 slider.addEventListener('click', themeChanger)
+sliderMob.addEventListener('click', themeChanger)
