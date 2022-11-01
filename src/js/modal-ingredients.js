cb2101915c12f64a073997bea07ad05a36fd4716
Enjoy3325 <enjoy3325@gmail.com>
2022-11-01 14:06:57 +0200

    update

--- src/js/modal-ingredients.js
+++ src/js/modal-ingredients.js
@@ -1,9 +1,14 @@
 import { templateModalIngredients } from './modal-template.js';
 const refs = {
-  closeModalBtn: document.querySelector('[data-modal-closes]'),
+  closeModal2Btn: document.querySelector('[data-modal-closes]'),
   addFavoriteBtn: document.querySelector('[data-add-favorite]'),
-  modal: document.querySelector('[data-modal]'),
+  modal2: document.querySelector('[data-modal]'),
   gallery: document.querySelector('.gallery'),
   backdrop: document.querySelector('.backdrop'),
   ingridientsList: document.querySelector('[data-modal-open]'),
 };
+
+function onCloseModal() {
+  refs.modal2.classList.add('is-hidden');
+  document.body.classList.toggle('no-scroll');
+}
