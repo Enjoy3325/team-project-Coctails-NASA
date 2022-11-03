40b874cb0f4b782c4c61e77894486fd50abf5d6a
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-11-03 14:09:09 +0200

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { child, get, getDatabase, ref, set, update} from 'firebase/database';
+import { child, get, getDatabase, ref, set, update } from 'firebase/database';
 import {
   createUserWithEmailAndPassword,
   getAuth,
@@ -18,7 +18,8 @@ const firebaseConfig = {
   storageBucket: 'cocktails-app-33ae0.appspot.com',
   messagingSenderId: '548673926993',
   appId: '1:548673926993:web:d83632f9f14fd47ab98570',
-  databaseURL: 'https://cocktails-app-33ae0-default-rtdb.europe-west1.firebasedatabase.app/',
+  databaseURL:
+    'https://cocktails-app-33ae0-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 // Initialize Firebase
@@ -37,62 +38,71 @@ function createUserData(name, email, userId) {
   set(ref(database, 'users/' + userId), {
     username: name,
     email: email,
-    favoriteCocktails: [{favoriteCocktailsArr: "qwerty" }],
-    favoriteIngredients: [{favoriteCocktailsArr: "qwerty" }],
-  }).then(() => {
-    updateProfile(auth.currentUser, {
-      displayName: name,
-    }).then(() => Notiflix.Notify.info(`User ${name} created`))
-      .catch((error) => {
-        const errorMessage = error.message;
-        Notiflix.Notify.failure(errorMessage);
-      });
+    favoriteCocktails: [{ favoriteCocktailsArr: 'qwerty' }],
+    favoriteIngredients: [{ favoriteCocktailsArr: 'qwerty' }],
   })
-    .then(()=>  getUserCocktails());
+    .then(() => {
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+        .then(() => Notiflix.Notify.info(`User ${name} created`))
+        .catch(error => {
+          const errorMessage = error.message;
+          Notiflix.Notify.failure(errorMessage);
+        });
+    })
+    .then(() => getUserCocktails());
 }
 
 function getUserCocktails() {
   const dbRef = ref(getDatabase());
   const userId = localStorage.getItem('userId');
-  get(child(dbRef, 'users/' + userId))
-    .then((snapshot) => {
-        if (snapshot.exists()) {
-          localStorage.setItem('favoriteCocktails', JSON.stringify(snapshot.val().favoriteCocktails))
-          localStorage.setItem('favoriteIngredients', JSON.stringify(snapshot.val().favoriteIngredients))
-        } else {
-          alert('No data found');
-        }
-      },
-    );
+  get(child(dbRef, 'users/' + userId)).then(snapshot => {
+    if (snapshot.exists()) {
+      console.log('snapshot', snapshot, snapshot.val());
+
+      const favoriteCocktails = snapshot
+        .val()
+        .favoriteCocktails?.filter(el => el.favoriteCocktailsArr !== 'qwerty');
+      localStorage.setItem(
+        'favoriteCocktails',
+        JSON.stringify(favoriteCocktails)
+      );
+      localStorage.setItem(
+        'favoriteIngredients',
+        JSON.stringify(favoriteCocktails)
+      );
+    } else {
+      alert('No data found');
+    }
+  });
 }
 
 // getUserCocktails()
 
-
-
 export function updateUserCocktails() {
   const userId = localStorage.getItem('userId');
   update(ref(database, 'users/' + userId), {
-    favoriteCocktails: JSON.parse(localStorage.getItem('favoriteCocktails') || '[]'),
-  })
-    .then(() => {
-      Notiflix.Notify.info(`User data updated`)
-    });
+    favoriteCocktails: JSON.parse(
+      localStorage.getItem('favoriteCocktails') || '[]'
+    ),
+  }).then(() => {
+    Notiflix.Notify.info(`User data updated`);
+  });
 }
 
 export function updateUserIngredients() {
   const userId = localStorage.getItem('userId');
   update(ref(database, 'users/' + userId), {
-    favoriteIngredients: JSON.parse(localStorage.getItem('favoriteIngredients') || '[]'),
-  })
-    .then(() => {
-      Notiflix.Notify.info(`User data updated`)
-    });
+    favoriteIngredients: JSON.parse(
+      localStorage.getItem('favoriteIngredients') || '[]'
+    ),
+  }).then(() => {
+    Notiflix.Notify.info(`User data updated`);
+  });
 }
 
 // updateUserCocktails()
-
-
 
 registerForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -100,12 +110,14 @@ registerForm.addEventListener('submit', e => {
   let email = e.target[1].value;
   let password = e.target[2].value;
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(userCredential => {
       const userId = userCredential.user.uid;
       createUserData(name, email, userId);
     })
-    .then(() => document.querySelector('[data-auth-modal]').classList.add('is-hidden'))
-    .catch((error) => {
+    .then(() =>
+      document.querySelector('[data-auth-modal]').classList.add('is-hidden')
+    )
+    .catch(error => {
       const errorMessage = error.message;
       Notiflix.Notify.failure(errorMessage);
     });
@@ -116,18 +128,20 @@ loginForm.addEventListener('submit', e => {
   let email = e.target[0].value;
   let password = e.target[1].value;
   signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(userCredential => {
       const user = userCredential.user.displayName;
       Notiflix.Notify.info(`Welcome, ${user}! U r in`);
     })
-    .then(() => document.querySelector('[data-auth-modal]').classList.add('is-hidden'))
-    .catch((error) => {
+    .then(() =>
+      document.querySelector('[data-auth-modal]').classList.add('is-hidden')
+    )
+    .catch(error => {
       const errorMessage = error.message;
       Notiflix.Notify.failure(errorMessage);
     });
 });
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, user => {
   if (user) {
     localStorage.setItem('userId', user.uid);
     getUserCocktails();
@@ -140,7 +154,9 @@ onAuthStateChanged(auth, (user) => {
     logoutBtn.insertAdjacentHTML('afterbegin', user.displayName);
     logoutBtnMob.insertAdjacentHTML('afterbegin', user.displayName);
     localStorage.setItem('isAuth', 'true');
-    document.querySelectorAll('.btn--white').forEach(item => item.classList.remove('is-hidden'))
+    document
+      .querySelectorAll('.btn--white')
+      .forEach(item => item.classList.remove('is-hidden'));
   } else {
     // updateUserCocktails();
     logoutBtn.classList.add('is-hidden');
@@ -151,35 +167,32 @@ onAuthStateChanged(auth, (user) => {
     logoutBtnMob.innerHTML = '';
     localStorage.setItem('isAuth', 'false');
     localStorage.setItem('userId', '');
-    document.querySelectorAll('.btn--white').forEach(item => item.classList.add('is-hidden'))
+    document
+      .querySelectorAll('.btn--white')
+      .forEach(item => item.classList.add('is-hidden'));
   }
 });
 
 logoutBtn.addEventListener('click', () => {
-  signOut(auth).then(
-    () => {
+  signOut(auth)
+    .then(() => {
       Notiflix.Notify.info(`U r out`);
-    },
-  ).catch((error) => {
+    })
+    .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage);
-    },
-  );
+    });
 });
 
 logoutBtnMob.addEventListener('click', () => {
-  signOut(auth).then(
-    () => {
+  signOut(auth)
+    .then(() => {
       Notiflix.Notify.info(`U r out`);
-    },
-  ).catch((error) => {
+    })
+    .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert(errorMessage);
-    },
-  );
+    });
 });
-
-
-
