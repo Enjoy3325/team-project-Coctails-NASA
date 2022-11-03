71483bfa684613fd66c461ac7f04221553062450
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-11-03 20:10:39 +0200

    Update request-by-letter.js

--- src/js/request-by-letter.js
+++
@@ -1,6 +0,0 @@
-import { requestApi } from './requests-api.js';
-// console.log(requestApi('a', 'letter'));
-
-const letterBox = document.querySelector('.hero__letter-box');
-
-// letterBox.addEventListener('click');
